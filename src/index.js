e9fc63d989192daae71693fff606534c181c3042
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2020-05-29 17:02:28 +0100

    perf: removes polyfills and auth0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // Polyfills
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import 'resize-observer-polyfill';
+// import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/stable';
+// import 'resize-observer-polyfill';
 
 import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import ProtectedRoute from './components/ProtectedRoute';
-import AuthCallback from './pages/AuthCallback';
+// import ProtectedRoute from './components/ProtectedRoute';
+// import AuthCallback from './pages/AuthCallback';
 import HomePage from './pages/HomePage';
 import MapPage from './pages/MapPage';
 import NotFound from './pages/404';
@@ -18,7 +18,7 @@ import PageLoading from './components/PageLoading';
 
 import history from './history';
 import Router from './Router';
-import AuthProvider from './Auth';
+// import AuthProvider from './Auth';
 import App from './App';
 
 const Explorer = lazy(() =>
@@ -54,48 +54,40 @@ const CookiesPage = lazy(() =>
 );
 
 ReactDOM.render(
-  <AuthProvider>
-    <App>
-      <Router history={history} forceRefresh={false}>
-        <Tracking />
-        <Suspense fallback={<PageLoading />}>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <ProtectedRoute path="/loos/add" component={AddPage} />
-            <Route path="/loos/:id" exact component={HomePage} />
-            <Route exact path="/about" component={AboutPage} />
-            <Route exact path="/cookies" component={CookiesPage} />
-            <Route exact path="/privacy" component={PrivacyPage} />
-            <Route exact path="/use-our-loos" component={UseOurLoosPage} />
-            <Route exact path="/contact" component={ContactPage} />
-            <Route
-              path="/contribute"
-              render={(props) => <ContributePage {...props} />}
-            />
-            <Router
-              path="/login"
-              render={() => <Redirect to="/contribute" />}
-            />
-            <Route
-              path="/map/:lng/:lat"
-              render={(props) => <MapPage {...props} />}
-            />
-            <Route
+  <App>
+    <Router history={history} forceRefresh={false}>
+      <Tracking />
+      <Suspense fallback={<PageLoading />}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {/* <ProtectedRoute path="/loos/add" component={AddPage} /> */}
+          <Route path="/loos/:id" exact component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/cookies" component={CookiesPage} />
+          <Route exact path="/privacy" component={PrivacyPage} />
+          <Route exact path="/use-our-loos" component={UseOurLoosPage} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route
+            path="/contribute"
+            render={(props) => <ContributePage {...props} />}
+          />
+          <Router path="/login" render={() => <Redirect to="/contribute" />} />
+          <Route
+            path="/map/:lng/:lat"
+            render={(props) => <MapPage {...props} />}
+          />
+          {/* <Route
               exact
               path="/callback"
               render={(props) => <AuthCallback {...props} />}
-            />
-            <Route
-              path="/explorer"
-              render={(props) => <Explorer {...props} />}
-            />
-            <ProtectedRoute path="/loos/:id/edit" component={EditPage} />
-            <ProtectedRoute path="/loos/:id/remove" component={RemovePage} />
-            <Route component={NotFound} />
-          </Switch>
-        </Suspense>
-      </Router>
-    </App>
-  </AuthProvider>,
+            /> */}
+          <Route path="/explorer" render={(props) => <Explorer {...props} />} />
+          {/* <ProtectedRoute path="/loos/:id/edit" component={EditPage} />
+            <ProtectedRoute path="/loos/:id/remove" component={RemovePage} /> */}
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Router>
+  </App>,
   document.getElementById('root')
 );
